64399fa6861b573e7438f2d789e023aed24873b3
Sergey Dmitriev <serge.dmitriev@gmail.com>
2018-01-17 00:48:15 +0700

    fix using file

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
 const express = require('express');
-const bodyparser = require('body-parser');
-const fs = require('fs-extra');
 const Joi = require('joi');
 const console = require('tracer').colorConsole();
-const basicAuth = require('express-basic-auth')
 
 const configSchema = require('./lib/configSchema');
 const Kue = require('./lib/kue');
 const Db = require('./lib/db');
-
-const HandlerRequest = require('./lib/handlers/request');
-const HandlerStatus = require('./lib/handlers/status');
-const HandlerFile = require('./lib/handlers/file');
+const createApp = require('./lib/index');
 
 const server = (config) => {
 
     let app, conn, kue;
 
     let init = (config) => {
-        app = express();
-        app.use(bodyparser.json());
+        
 
-        //auth
-        if (config.auth && config.auth.users) {
-            app.use(basicAuth({
-                users: config.auth.users
-            }))
-        }
+
 
         conn = Db(config);
         kue = new Kue({conn, config});
+
+        app = createApp({conn, kue, config});
+        /*
+        app.use(bodyparser.json());
 
         var handlerRequest = HandlerRequest({conn, kue, config});
         var handlerStatus = HandlerStatus({conn});
@@ -38,14 +30,11 @@ const server = (config) => {
         app.use('/api/key', handlerRequest.router());
         app.use('/api/log', handlerRequest.router());
 
-        app.use('/api/task', handlerRequest.router((req, res, next) => {
-            req.sendToKue = true;
-            next();
-        }));
+        app.use('/api/task', 
         app.use('/api/task', handlerStatus.router());
 
         app.use('/api/file/', handlerFile.router());
-
+        */
     };
 
     Joi.validate(config, configSchema, {allowUnknown: true}, (err, config) => {
